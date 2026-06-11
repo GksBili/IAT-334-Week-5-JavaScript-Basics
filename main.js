b5f3308019b362fd22ba1b9f1a71857274dd18e3
GksBili <146159659+GksBili@users.noreply.github.com>
2026-06-11 09:47:04 -0700

    Add simple math quiz to main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let question = "What does 4+5 equal to?";
+
+let answer = prompt(question);
+
+if (answer == 9) {
+    alert("Correct!");
+} else {
+    alert("Incorrect. The correct answer is 9.");
+}
